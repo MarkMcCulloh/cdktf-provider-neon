84f380bf6d1accf1e55500461822266575d62fff
Chris Rybicki <chrisr@monada.co>
2023-11-10 19:24:06 -0500

    update config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,19 +1,18 @@
 import { CdktfProviderProject } from "@cdktf/provider-project";
+
 const project = new CdktfProviderProject({
   author: "Chris Rybicki",
-  authorAddress: "chrisr@monada.co",
-  cdktfVersion: 0.19,
-  constructsVersion: "10.3.0",
+  authorAddress: "crybicki98@gmail.com",
+  cdktfVersion: "^0.19.0",
+  constructsVersion: "^10.3.0",
   defaultReleaseBranch: "main",
-  devDeps: ["@cdktf/provider-project"],
-  jsiiVersion: "~5.0.0",
-  name: "provider-neon",
+  devDeps: ["@cdktf/provider-project@^0.3.0"],
+  jsiiVersion: "^5.0.0",
+  name: "@rybickic/provider-neon",
   projenrcTs: true,
   repositoryUrl: "https://github.com/chrisr/provider-neon.git",
   terraformProvider: "kislerdm/neon@~> 0.2",
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  description: "CDKTF provider for kislerdm/neon",
 });
+
 project.synth();
